2f20972bc621a742432b59fcaff0a12222a49049
kiran <kiranresearch2022@gmail.com>
2023-05-20 17:08:26 +0530

    changes to C.java

--- C.java
+++ C.java
@@ -2,9 +2,13 @@ class C {
     method20() {
          System.out.println("in method20");
     }
+   
+    method120() {
+	System.out.println("in method120");
+   }
 
-    method21() {
-         System.out.println("in method21");
+    method29() {
+         System.out.println("in method29");
     }
 
     
